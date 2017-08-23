9fa934658c7151cca8b73281f604a0ae2a4b856e
wulinzhu <wulinzhu1954@gmail.com>
2017-08-23 13:21:42 +0800

    Define metadata of player in DB; add operation create and find account in DB; Add DB configuration file

---
+++ src/server/account_handler.js
@@ -0,0 +1,62 @@
+function account_handler() {}
+
+account_handler.mongo_client = require("mongodb").MongoClient;
+
+account_handler.mongo_config = require("./db_config");
+
+var player_data = require("./player_data");
+
+// mongo url, example: mongodb://user:password@127.0.0.1/dbName
+account_handler.mongo_url = "mongodb://" + account_handler.mongo_config.user + ":" +
+  account_handler.mongo_config.passwd + "@" +
+  account_handler.mongo_config.ip + ":" +
+  account_handler.mongo_config.port + "/" +
+  account_handler.mongo_config.db;
+
+account_handler.agario_db = null;
+
+account_handler.init = function (callback) {
+  if (account_handler.agario_db) {
+    callback(null, account_handler.agario_db);
+    return;
+  }
+
+  console.log("Connect to " + account_handler.mongo_url);
+  account_handler.mongo_client.connect(account_handler.mongo_url, function (err, db) {
+    account_handler.agario_db = db;
+    db.on('close', function () {
+      delete(account_handler.agario_db);
+    })
+    callback(err, db);
+  });
+};
+
+account_handler.create = function (id, passwd, name, callback) {
+  account_handler.init(function (error, db) {
+    var player_set = db.collection("player");
+    var data = new player_data(id, name, passwd);
+
+    if (typeof (id) != "string" || typeof (passwd) != "string" ||
+      typeof (name) != "string") {
+      callback(-1, null);
+    }
+    console.log("Add new user: " + id + ":" + name);
+    player_set.insert(data, callback);
+  });
+};
+
+account_handler.find = function (id, callback) {
+  account_handler.init(function (error, db) {
+    var player_set = db.collection("player");
+    player_set.find({
+      "_id": id
+    }).toArray(callback);
+  });
+};
+
+account_handler.close = function () {
+  if (account_handler.agario_db) {
+    account_handler.agario_db.close();
+  }
+};
+module.exports = account_handler;
---
+++ src/server/player_data.js
@@ -0,0 +1,12 @@
+function player_data(id, nick_name, pwd) {
+  return {
+    _id: id,
+    name: nick_name,
+    passwd: pwd,
+    weight: 0,
+    nr_game: 0,
+    nr_kill: 0,
+  }
+};
+
+module.exports = player_data;
