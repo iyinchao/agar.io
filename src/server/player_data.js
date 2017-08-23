95e3038b51c5ba84831d21251b2c4a80eb5547d4
wulinzhu <wulinzhu1954@gmail.com>
2017-08-23 20:46:50 +0800

    Update metadata of player_data

--- src/server/player_data.js
+++ src/server/player_data.js
@@ -1,12 +1,13 @@
-function player_data(id, nick_name, pwd, mass, game, kill) {
-  return {
-    _id: id,
-    name: nick_name,
-    passwd: pwd,
-    weight: mass,
-    nr_game: game,
-    nr_kill: kill,
-  };
+function player_data(id, nick_name, pwd, img, mass, game, kill) {
+    return {
+        _id: id,
+        name: nick_name,
+        passwd: pwd,
+        image_url: img,
+        weight: mass,
+        nr_game: game,
+        nr_kill: kill,
+    };
 }
 
 module.exports = player_data;
